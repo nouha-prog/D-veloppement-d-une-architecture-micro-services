3f505d3bd62419b34e06e9f136041db76716ea1a
Nouhaila <nouhailaelhajji488@gmail.com>
2025-10-13 11:20:28 +0100

    First commit : Creation de Customer-service

---
+++ src/main/java/org/example/ecomiibdccapp/EcomIiBdccAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecomiibdccapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomIiBdccAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomIiBdccAppApplication.class, args);
+    }
+
+}
